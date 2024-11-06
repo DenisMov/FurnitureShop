9bf7ce4740275bd9fe4e08abd57be32fbf2c6b6c
den <viewsonic352@gmail.com>
2024-11-06 20:09:19 +0200

    change server, package.json \ not working on vercel

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 // api/server.js
 const express = require("express");
+const cors = require("cors"); // Підключаємо CORS
 const app = express();
+
+app.use(cors()); // Включаємо CORS для всіх маршрутів
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Node.js server!" });
 });
 
-// Експортуємо `app`, щоб Vercel міг використовувати його як обробник
 module.exports = app;
