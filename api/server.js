a6a0fb263229d43313f43cf34b295a28d9f9f528
den <viewsonic352@gmail.com>
2024-11-06 20:24:11 +0200

    server.js update \ not working on vercel

--- api/server.js
+++ api/server.js
@@ -1,9 +1,5 @@
-// api/server.js
 const express = require("express");
-const cors = require("cors"); // Підключаємо CORS
 const app = express();
-
-app.use(cors()); // Включаємо CORS для всіх маршрутів
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Node.js server!" });
