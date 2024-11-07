8056fc429c5d82378bde1053fa7bf667f31dde8f
den <viewsonic352@gmail.com>
2024-11-07 12:50:28 +0200

    change hook.ts

--- src/hooks/http.hook.ts
+++ src/hooks/http.hook.ts
@@ -12,20 +12,23 @@ interface IRequestConfig {
 }
 
 export const useHttp = () => {
-  const request = async ({
+  const request = async <T = any>({
     url,
     method = "GET",
     body = null,
     headers = { "Content-Type": "application/json" },
-  }: IRequestConfig) => {
+  }: IRequestConfig): Promise<T> => {
     try {
       const config: RequestInit = {
         method,
-        headers,
+        headers: new Headers(headers),
       };
 
       if (body) {
         config.body = typeof body === "string" ? body : JSON.stringify(body);
+        if (!(headers && headers["Content-Type"])) {
+          headers["Content-Type"] = "application/json";
+        }
       }
 
       const response = await fetch(url, config);
@@ -34,10 +37,10 @@ export const useHttp = () => {
         throw new Error(`Request failed with status code ${response.status}`);
       }
 
-      const data = await response.json();
-
+      const data: T = await response.json();
       return data;
     } catch (e) {
+      console.error("HTTP Request Error:", e);
       throw e;
     }
   };
