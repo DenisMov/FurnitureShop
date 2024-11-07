b157309076b065c99bd5996571dc2522d5cc7c3f
den <viewsonic352@gmail.com>
2024-11-07 11:11:14 +0200

    change  server.js + vercel API - server not working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/products", (req, res) => {
+  console.log("Vercel");
   res.json(products); // припустимо, що `products` - це ваш масив із даними
 });
 
